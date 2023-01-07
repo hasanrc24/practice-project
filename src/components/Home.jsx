2e2f75e5b976197963978c8b738b8ff265d7cc2f
hasanrc24 <hasanrc24@gmail.com>
2023-01-07 12:40:43 +0600

    design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+ import {IoIosSend} from 'react-icons/io'
+ import {MdDelete} from 'react-icons/md'
 
 const Home = () => {
   return (
-    <div className='h-[70%] w-[25%] bg-red-300 rounded-lg'>Home</div>
+    <div className='h-[80%] w-[60%] md:w-[30%] shadow-2xl bg-gray-300 rounded-lg '>
+        <p className='text-center mt-4 text-xl'>Todo List</p>
+      <div className="p-4">
+        <div className="w-full mb-4 flex items-center">
+          <input type="text" name="todo" className='w-full p-2 outline-none border-b-2 border-b-gray-600 focus:border-b-purple-600' placeholder='Enter your task' />
+          <button className='ml-2 p-2 text-2xl bg-indigo-300'><IoIosSend /></button>
+        </div>
+        <div className="flex justify-between bg-slate-400 my-1 p-2">
+          <input type="checkbox" name="check" className='m-2 p-4 h-4 w-4' />
+          <p>Lorem ipsum asdjfhad fashdfa sdfh as</p>
+          <button className='text-xl'> <MdDelete /></button>
+        </div>
+        <div className="flex justify-between bg-slate-400 my-1 p-2">
+          <input type="checkbox" name="check" className='m-2 p-4 h-4 w-4' />
+          <p>Lorem ipsum</p>
+          <button className='text-xl'> <MdDelete /></button>
+        </div>
+      </div>
+    </div>
   )
 }
 
