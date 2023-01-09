6632a83fa16136ac14d0b0888d8c84add13fe5fa
hasanrc24 <hasanrc24@gmail.com>
2023-01-09 23:54:04 +0600

    handle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
  import {IoIosSend} from 'react-icons/io'
  import {MdDelete} from 'react-icons/md'
 
 const Home = () => {
+  const [todo, setTodo] = useState('');
+  console.log(todo)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo('');
+  }
   return (
     <div className='h-[80%] w-[60%] md:w-[30%] shadow-2xl bg-gray-300 rounded-lg '>
         <p className='text-center mt-4 text-xl'>Todo List</p>
       <div className="p-4">
-        <div className="w-full mb-4 flex items-center">
-          <input type="text" name="todo" className='w-full p-2 outline-none border-b-2 border-b-gray-600 focus:border-b-purple-600' placeholder='Enter your task' />
-          <button className='ml-2 p-2 text-2xl bg-indigo-300'><IoIosSend /></button>
-        </div>
+        <form className="w-full mb-4 flex items-center" onSubmit={handleSubmit}>
+          <input type="text" name="todo" value={todo} onChange={(e) => setTodo(e.target.value)} className='w-full p-2 outline-none border-b-2 border-b-gray-600 focus:border-b-purple-600' placeholder='Enter your task' />
+          <button type='submit' className='ml-2 p-2 text-2xl bg-indigo-300'><IoIosSend /></button>
+        </form>
         <div className="flex justify-between bg-slate-400 my-1 p-2">
           <input type="checkbox" name="check" className='m-2 p-4 h-4 w-4' />
           <p>Lorem ipsum asdjfhad fashdfa sdfh as</p>
